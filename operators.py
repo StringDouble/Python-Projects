0ffabec4a00702f496912367ed4b3364f7269dd8
Yoana <yoana@Yoanas-MacBook-Air.local>
2018-06-29 22:22:14 +0300

    update

---
+++ operators.py
@@ -0,0 +1,24 @@
+a = 10
+b = 8
+c = a - b
+print(c < 12)
+print(c / a + 1)
+print(b ** a)
+a -= c
+print(c // b)                                                                          
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
